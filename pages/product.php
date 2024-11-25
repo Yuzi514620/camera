e83eb3b1f396c702efdd961ce3b299ff4ccfb12b
PK <hanhan010609@gmail.com>
2024-11-25 15:35:03 +0800

    側邊欄統一

--- pages/product.php
+++ pages/product.php
@@ -39,87 +39,10 @@
 </head>
 
 <body class="g-sidenav-show bg-gray-100">
-  <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2  bg-dark my-2"
-    id="sidenav-main">
-    <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-        aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand px-4 py-3 m-0" href=" # "
-        target="_blank">
-        <!-- <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img" width="26" height="26" alt="main_logo"> -->
-        <span class="ms-1 text-sm text-white">Camera</span>
-        </a>
-      </div>
-      <hr class="horizontal dark mt-0 mb-2" />
-      <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link text-white" href="../pages/users.php">
-            <i class="fa-solid fa-user"></i>
-            <span class="nav-link-text ms-1">會員管理</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../pages/order.php">
-          <i class="fa-solid fa-file"></i>
-            <span class="nav-link-text ms-1">訂單管理</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active bg-white text-dark" href="../pages/product.php">
-            <i class="fa-solid fa-camera"></i>
-            <span class="nav-link-text ms-1">商品管理</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../pages/rental.php">
-            <i class="fa-solid fa-handshake"></i>
-            <span class="nav-link-text ms-1">租借商品管理</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../pages/course.php">
-            <i class="fa-solid fa-book"></i>
-            <span class="nav-link-text ms-1">課程管理</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../pages/coupon.php">
-            <i class="fa-solid fa-ticket"></i>
-            <span class="nav-link-text ms-1">優惠券管理</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="../pages/article.php">
-            <i class="fa-solid fa-book-open"></i>
-            <span class="nav-link-text ms-1">文章管理</span>
-          </a>
-        </li>
-        <!-- <li class="nav-item mt-3">
-          <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Account pages</h6>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/profile.html">
-            <i class="material-symbols-rounded opacity-5">person</i>
-            <span class="nav-link-text ms-1">Profile</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/sign-in.html">
-            <i class="material-symbols-rounded opacity-5">login</i>
-            <span class="nav-link-text ms-1">Sign In</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/sign-up.html">
-            <i class="material-symbols-rounded opacity-5">assignment</i>
-            <span class="nav-link-text ms-1">Sign Up</span>
-          </a>
-        </li> -->
-      </ul>
-    </div>
-
-  </aside>
+  <!-- 側邊欄 -->
+  <?php $page = 'product'; ?>
+  <?php include 'sidebar.php'; ?>
+  <!-- 側邊欄 -->
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <!-- Navbar -->
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-3 shadow-none border-radius-xl" id="navbarBlur"
