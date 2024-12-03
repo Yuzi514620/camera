315480542fe6f7a7def243b8204ac2b2b1fd1927
Vincent <OxblackdesertxO@gmail.com>
2024-12-03 09:32:29 +0800

    改連結跟統一樣式

--- article/articleEdit.php
+++ article/articleEdit.php
@@ -125,62 +125,24 @@ try {
     <?php include 'sidebar.php'; ?>
   <!-- 側邊欄 -->
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-    <!-- Navbar -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-3 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
-      <div class="container-fluid py-1 px-3 justify-content-end">
-        <nav aria-label="breadcrumb">
-          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-            <li class="breadcrumb-item text-sm">
-              <a class="opacity-5 text-dark" href="javascript:;">Pages</a>
-            </li>
-            <li class="breadcrumb-item text-sm">
-              <a class="opacity-5 text-dark" href="article.php">文章列表</a>
-            </li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">
-              編輯文章
-            </li>
-          </ol>
-        </nav>
-        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-          <!-- 添加 ms-auto 將內容推向右側 -->
-          <ul class="navbar-nav d-flex align-items-center justify-content-end ms-auto">
-            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-                <div class="sidenav-toggler-inner">
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                </div>
-              </a>
-            </li>
-            <li class="nav-item px-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0">
-                <i class="material-symbols-rounded fixed-plugin-button-nav">settings</i>
-              </a>
-            </li>
-            <li class="nav-item dropdown pe-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="material-symbols-rounded">notifications</i>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-end px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
-                <!-- 通知內容 -->
-              </ul>
-            </li>
-            <li class="nav-item d-flex align-items-center">
-              <a href="../pages/sign-in.php" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa-solid fa-circle-user"></i>
-              </a>
-            </li>
-            <li class="nav-item d-flex align-items-center ms-3">
-              <a href="../pages/sign-in.php" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa-solid fa-right-from-bracket"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-   <!-- Navbar -->
+          <!-- Navbar -->
+          <?php
+          $breadcrumbs = [
+              'users' => '首頁',
+              'article' => '文章管理',
+              'articleEdit' => '編輯文章',
+          ];
+
+          $page = 'articleEdit';
+
+          $breadcrumbLinks = [
+              'users' => '../users/users.php',
+              'article' => 'article.php',
+              'articleEdit' => 'articleEdit.php',
+          ];
+
+          include '../navbar.php';
+          ?>
 
     <div class="container-fluid py-2">
       <div class="d-flex ps-5 mt-4 btn-back">
@@ -236,7 +198,7 @@ try {
 
                           <!-- 送出和返回按鈕 -->
                           <div class="d-flex mt-3 btn-under">
-                              <button type="submit" class="btn btn-sm btn-dark ms-auto me-1 align-content-center btn-send font-weight-bold" style="height:45px; border-radius:50%;">送出</button>
+                              <button type="submit" class="btn btn-sm btn-dark ms-auto me-1 align-content-center btn-send font-weight-bold" style="height:50px; border-radius:50%;">送出</button>
                               <a class="btn btn-sm btn-danger align-content-center font-weight-bold" style="height:45px; border-radius:50%;" href="article.php">取消</a>
                           </div>
                       </div>
